25e51ff03c246ddcc184abf61e488b430e095a6a
carlosgs <carlosgs.iesquevedo@gmail.com>
2025-10-01 18:13:16 +0200

    Commit inicial

---
+++ src/main/java/es/carlosgs/tarjetas/TarjetasApplication.java
@@ -0,0 +1,13 @@
+package es.carlosgs.tarjetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetasApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TarjetasApplication.class, args);
+    }
+
+}
